95019226e908af5a8c034883c8c0395916581254
William Enrique Caamal Caamal <a24216894@alumnos.uady.mx>
2025-09-02 19:46:11 +0000

    nuevo

---
+++ scr/org/ejercicio/controlescolar/Main.java
@@ -0,0 +1,8 @@
+
+package org.ejercicio.controlescolar;
+public class Main{
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan",12);
+        System.out.println("Hola "+persona1+" !");
+    }
+}
